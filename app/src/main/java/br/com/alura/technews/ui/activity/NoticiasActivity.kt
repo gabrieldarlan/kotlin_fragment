4088859bc6bd43d5ecbcb647e6ae646d56746d06
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-07-04 20:18:01 -0300

    cap 04 completo

--- app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
@@ -1,6 +1,7 @@
 package br.com.alura.technews.ui.activity
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -13,6 +14,8 @@ import br.com.alura.technews.ui.fragment.VisualizaNoticiaFragment
 
 private const val TITULO_APPBAR = "Notícias"
 
+private const val TAG_FRAGMENT_VISUALIZA_NOTICIA = "visualizaNoticia"
+
 class NoticiasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +24,36 @@ class NoticiasActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             abreListaNoticias()
+        } else {
+            supportFragmentManager.findFragmentByTag(TAG_FRAGMENT_VISUALIZA_NOTICIA)
+                ?.let { fragment ->
+                    val argumentos = fragment.arguments
+                    val novoFragment = VisualizaNoticiaFragment()
+                    novoFragment.arguments = argumentos
+
+                    transacaoFragment {
+                        remove(fragment)
+                    }
+
+                    supportFragmentManager.popBackStack()
+
+                    transacaoFragment {
+                        val container =
+                            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                                R.id.activity_noticias_container_secundario
+                            } else {
+                                addToBackStack(null)
+                                R.id.activity_noticias_container_primario
+                            }
+                        replace(container, novoFragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
+                    }
+                }
         }
     }
 
     private fun abreListaNoticias() {
         transacaoFragment {
-            replace(R.id.activity_noticias_container, ListaNoticiasFragment())
+            replace(R.id.activity_noticias_container_primario, ListaNoticiasFragment())
         }
     }
 
@@ -63,8 +90,14 @@ class NoticiasActivity : AppCompatActivity() {
         dados.putLong(NOTICIA_ID_CHAVE, noticia.id)
         fragment.arguments = dados
         transacaoFragment {
-            addToBackStack(null)
-            replace(R.id.activity_noticias_container, fragment)
+            val container =
+                if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    R.id.activity_noticias_container_secundario
+                } else {
+                    addToBackStack(null)
+                    R.id.activity_noticias_container_primario
+                }
+            replace(container, fragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
         }
     }
 
